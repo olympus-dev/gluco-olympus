9ac7de11a10d9bc978b79945197880c06f649199
Luan Lima <limaluan.dev@gmail.com>
2024-10-22 17:33:08 -0300

    feat: adds return policy page

---
+++ src/app/return-policy/page.tsx
@@ -0,0 +1,125 @@
+/* eslint-disable react/no-unescaped-entities */
+export default function ReturnPolicy() {
+  return (
+    <div>
+      <main className="container mx-auto mt-6">
+        <section>
+          <h1 className="text-5xl text-center mt-16">Return Policy</h1>
+        </section>
+
+        <section>
+          <h1 className="px-6 text-4xl mt-12 md:px-0">Returns </h1>
+          <p className="px-6 mt-8 leading-loose md:px-0">
+            If you are unsatisfied with your product for any reason you will
+            have 60 days from the date of purchase to return the product and
+            request a refund. You may return any Product (as defined below)
+            purchased on endoterec.online in accordance with the terms below: A
+            return authorization must be requested and approved within 60 days
+            of your date of purchase. You must contact us at
+            support@endoterec.com The return authorization number must be
+            included along with your returned product. Once an please return the
+            item and email us back with the and the tracking so we can track
+            your return. You will have to pay for shipping. You must return the
+            Product to the address provided to receive a refund at your cost
+            within 14 days. Returned Product(s) must be in good physical
+            condition (not physically broken or damaged). All accessories
+            originally included with your purchase must be included with your
+            return. For your convenience, the return address for Endoterec is:
+            7780 49th St North Box 513 Pinellas Park FL 33718
+          </p>
+        </section>
+
+        <section>
+          <h1 className="px-6 text-4xl mt-6 md:px-0">Non-returnable items:</h1>
+          <p className="px-6 mt-4 leading-loose md:px-0">
+            Gift cards Downloadable software products Some health and personal
+            care items To complete your return, we require a receipt or proof of
+            purchase. Please do not send your purchase back to the manufacturer.
+          </p>
+        </section>
+
+        <section>
+          <h1 className="px-6 text-4xl mt-6 md:px-0">
+            There are certain situations where only partial refunds are granted
+            (if applicable)
+          </h1>
+          <p className="px-6 mt-8 leading-loose md:px-0">
+            Book with obvious signs of use CD, DVD, VHS tape, software, video
+            game, cassette tape, or vinyl record that has been opened. Any item
+            not in its original condition, is damaged or missing parts for
+            reasons not due to our error Any item that is returned more than 60
+            days after purchase
+          </p>
+        </section>
+
+        <section>
+          <h1 className="px-6 text-4xl mt-6 md:px-0">
+            Refunds (if applicable)
+          </h1>
+          <p className="px-6 mt-8 leading-loose md:px-0">
+            Once your return is received and inspected, we will send you an
+            email to notify you that we have received your returned item. We
+            will also notify you of the approval or rejection of your refund. If
+            you are approved, then your refund will be processed, and a credit
+            will automatically be applied to your credit card or original method
+            of payment, within a certain amount of days.
+          </p>
+        </section>
+
+        <section>
+          <h1 className="px-6 text-4xl mt-6 md:px-0">
+            Late or missing refunds (if applicable)
+          </h1>
+          <p className="px-6 mt-8 leading-loose md:px-0">
+            If you haven’t received a refund yet, first check your bank account
+            again. Then contact your credit card company, it may take some time
+            before your refund is officially posted. Next contact your bank.
+            There is often some processing time before a refund is posted. If
+            you’ve done all of this and you still have not received your refund
+            yet, please contact us at support@endoterec.com
+          </p>
+        </section>
+
+        <section>
+          <h1 className="px-6 text-4xl mt-6 md:px-0">
+            Exchanges (if applicable)
+          </h1>
+          <p className="px-6 mt-8 leading-loose md:px-0">
+            We only replace items if they are defective or damaged. If you need
+            to exchange it for the same item, send us an email at
+            support@endoterec.com and we will provide you further instructions
+            on where to return the item for an exchange. Please note that we
+            cannot accept exchanges beyond 60 days from the date of delivery.
+          </p>
+        </section>
+
+        <section>
+          <h1 className="px-6 text-4xl mt-6 md:px-0">Gifts</h1>
+          <p className="px-6 mt-8 leading-loose md:px-0">
+            If the item was marked as a gift when purchased and shipped directly
+            to you, you’ll receive a gift credit for the value of your return.
+            Once the returned item is received, a gift certificate will be
+            mailed to you. If the item wasn’t marked as a gift when purchased,
+            or the gift giver had the order shipped to themselves to give to you
+            later, we will send a refund to the gift giver and he will find out
+            about your return. Still have questions about your return? Contact
+            customer service at support@endoterec.com
+          </p>
+        </section>
+      </main>
+      <footer className="text-center mt-16 py-10 bg-slate-100">
+        <div className="container mx-auto">
+          <p className="px-6 text-sm md:px-0">
+            The information on this website has not been evaluated by the Food
+            and Drug Administration. These products are not intended to
+            diagnose, treat, cure or prevent any disease.
+          </p>
+          <p className="px-6 mt-4 text-sm md:px-0">
+            © Endoterec Research 2024 All Rights Reserved. 7780 49th St North
+            Box 513 Pinellas Park FL 33718
+          </p>
+        </div>
+      </footer>
+    </div>
+  );
+}
