33e72c228b637f11df7f4f6474c8e816d1498367
Luan Lima <limaluan.dev@gmail.com>
2024-10-22 16:46:41 -0300

    feat: adds terms of service page

---
+++ src/app/terms-of-service/page.tsx
@@ -0,0 +1,219 @@
+/* eslint-disable react/no-unescaped-entities */
+export default function Disclaimer() {
+  return (
+    <div>
+      <main className="container mx-auto mt-6">
+        <section>
+          <h1 className="text-5xl text-center mt-16">Terms and Conditions</h1>
+          <p className="px-6 mt-16 leading-loose md:px-0">
+            These terms and conditions outline the rules and regulations for the
+            use of this website.
+          </p>
+        </section>
+
+        <section>
+          <h1 className="px-6 text-4xl mt-12 md:px-0">INTRODUCTION </h1>
+          <p className="px-6 mt-8 leading-loose md:px-0">
+            This Agreement contains the complete terms and conditions that apply
+            to you in joining, buying, bidding, selling and all other activities
+            you will make in our website. By using or shopping from this Web
+            site, you agree to be bound by its terms of use and shall comply
+            thereof. This Agreement describes and encompasses the entire
+            agreement between us and you, and supersedes all prior or
+            contemporaneous agreements, representations, warranties and
+            understandings with respect to the Site, the content and computer
+            programs provided by or through the Site, and the subject matter of
+            this Agreement. Amendments to this agreement can be made and
+            effected by us from time to time without specific notice to you end.
+            Agreement posted on the Site reflects the latest agreement and you
+            should carefully review the same before you use our site.
+          </p>
+        </section>
+
+        <section>
+          <h1 className="px-6 text-4xl mt-6 md:px-0">
+            Use of the site & PROHIBITIONS
+          </h1>
+          <p className="px-6 mt-4 leading-loose md:px-0">
+            The Site allows you to post offers, sell, advertise, bid and shop
+            online. However, you are prohibited to do the following acts, to
+            wit: (a) use our sites, including its services and or tools if you
+            are not able to form legally binding contracts, are under the age of
+            18, or are temporarily or indefinitely suspended from using our
+            sites, services, or tools (b) posting of an items in inappropriate
+            category or areas on our sites and services; (c) collecting
+            information about users' personal information; (d) maneuvering the
+            price of any item or interfere with other users' listings; (f) post
+            false, inaccurate, misleading, defamatory, or libelous content; (g)
+            take any action that may damage the rating system. For you to
+            complete the sign-up process in our site, you must provide your full
+            legal name, current address, a valid email address, and any other
+            information needed in order to complete the signup process. You must
+            qualify that you are 18 years or older and must be responsible for
+            keeping your password secure and be responsible for all activities
+            and contents that are uploaded under your account. You must not
+            transmit any worms or viruses or any code of a destructive nature.
+          </p>
+        </section>
+
+        <section>
+          <h1 className="px-6 text-4xl mt-6 md:px-0">
+            Payments AND PROCESSES OF INVIOCES
+          </h1>
+          <p className="px-6 mt-8 leading-loose md:px-0">
+            LINK DO PRODUTO has the sole discretion to
+            provide the terms of payment. Unless otherwise agreed, payment must
+            first be received by LINK DO PRODUTO the latter's
+            acceptance of an order. Unless credit term has been agreed upon,
+            payment for the products shall be made by credit card, paypal or
+            wire transfers. Invoices are due and payable within the time period
+            noted on your invoice, measured from the date of the invoice. An
+            order may be invoice separately. LINK DO PRODUTO
+            has all the discretion to cancel or deny orders.
+            LINK DO PRODUTO is not responsible for pricing,
+            typographical, or other errors in any offer by
+            LINK DO PRODUTO and reserves the right to cancel
+            any orders arising from such errors. Invoices must be paid within 15
+            days of the invoice date. For all but consumer purchases,
+            LINK DO PRODUTO reserves the right to charge you
+            a late penalty charge of 1% per month applied against undisputed
+            overdue amounts or the maximum rate permitted by law whichever is
+            less. Every 30 days thereafter, you will continue to be charged an
+            additional late penalty charge.
+          </p>
+        </section>
+
+        <section>
+          <h1 className="px-6 text-4xl mt-6 md:px-0">RISK OF LOSS</h1>
+          <p className="px-6 mt-8 leading-loose md:px-0">
+            All items purchased from our website are made pursuant to a shipment
+            contract. The risk of loss and title for such items pass to you upon
+            our delivery to the carrier
+          </p>
+        </section>
+
+        <section>
+          <h1 className="px-6 text-4xl mt-6 md:px-0">
+            Editing, Deleting and Modification
+          </h1>
+          <p className="px-6 mt-8 leading-loose md:px-0">
+            We may edit, delete or modify any of the terms and conditions
+            contained in this Agreement, at any time and in our sole discretion,
+            by posting a notice or a new agreement on our site. YOUR CONTINUED
+            PARTICIPATION IN OUR PROGRAM, VISIT AND SHOPPING IN OUR SITE
+            FOLLOWING OUR POSTING OF A CHANGE NOTICE OR NEW AGREEMENT ON OUR
+            SITE WILL CONSTITUTE BINDING ACCEPTANCE OF THE CHANGE.
+          </p>
+        </section>
+
+        <section>
+          <h1 className="px-6 text-4xl mt-6 md:px-0">
+            Acknowledgment of rights
+          </h1>
+          <p className="px-6 mt-8 leading-loose md:px-0">
+            You hereby acknowledge that all rights, titles and interests,
+            including but not limited to rights covered by the Intellectual
+            Property Rights, in and to the site, and that You will not acquire
+            any right, title, or interest in or to the Program except as
+            expressly set forth in this Agreement. You will not modify, adapt,
+            translate, prepare derivative works from, decompile, reverse
+            engineer, disassemble or otherwise attempt to derive source code
+            from any of our services, software, or documentation, or create or
+            attempt to create a substitute or similar service or product through
+            use of or access to the Program or proprietary information related
+            thereto.
+          </p>
+        </section>
+
+        <section>
+          <h1 className="px-6 text-4xl mt-6 md:px-0">Fraud</h1>
+          <p className="px-6 mt-8 leading-loose md:px-0">
+            FRAUDULENT ACTIVITIES are highly monitored in our site and if fraud
+            is detected LINK DO PRODUTO shall resort al
+            remedies available to us, and you shall be responsible for all costs
+            and legal fees arising from these fraudulent activities.
+          </p>
+        </section>
+
+        <section>
+          <h1 className="px-6 text-4xl mt-6 md:px-0">MISCELLANEOUS</h1>
+          <p className="px-6 mt-8 leading-loose md:px-0">
+            This Agreement shall be governed by and construed in accordance with
+            the substantive laws of USA, without any reference to
+            conflict-of-laws principles. Any dispute, controversy or difference
+            which may arise between the parties out of, in relation to or in
+            connection with this Agreement is hereby irrevocably submitted to
+            the exclusive jurisdiction of the courts of US, to the exclusion of
+            any other courts without giving effect to its conflict of laws
+            provisions or your actual state or country of residence. The entire
+            agreement between the parties with respect to the subject matter
+            hereof is embodied on this agreement and no other agreement relative
+            hereto shall bind either party herein. Your rights of whatever
+            nature cannot be assigned nor transferred to anybody, and any such
+            attempt may result in termination of this Agreement, without
+            liability to us. However, we may assign this Agreement to any person
+            at any time without notice. In the event that any provision of these
+            Terms and Conditions is found invalid or unenforceable pursuant to
+            any judicial decree or decision, such provision shall be deemed to
+            apply only to the maximum extent permitted by law, and the remainder
+            of these Terms and Conditions shall remain valid and enforceable
+            according to its terms.
+          </p>
+        </section>
+
+        <section>
+          <h1 className="px-6 text-4xl mt-6 md:px-0">
+            PRIVACY POLICY IS PART OF THESE TERMS AND CONDITIONS
+          </h1>
+          <p className="px-6 mt-8 leading-loose md:px-0">
+            Our privacy policy is part of, and subject to, these terms and
+            conditions of use.
+          </p>
+        </section>
+
+        <section>
+          <h1 className="px-6 text-4xl mt-6 md:px-0">CONTENT OWNERSHIP </h1>
+          <p className="px-6 mt-8 leading-loose md:px-0">
+            All content on our website is owned by us or our content suppliers.
+            On behalf of ourselves and our content suppliers, we claim all
+            property rights, including intellectual property rights, for this
+            content and you are not allowed to infringe upon those rights. We
+            will prosecute to the fullest extent of the law anyone who attempts
+            to steal our property. You agree not to copy content from our
+            website without our permission. Any requests to use our content
+            should be submitted to us by e-mail to <b>support@endoterec.com</b>.
+            If you believe that your intellectual property rights have been
+            infringed upon by our website content, please notify us by sending
+            an e-mail to
+            <b>support@endoterec.com</b>, or by sending mail to us at the
+            address listed below. Please describe in detail the alleged
+            infringement, including the factual and legal basis for your claim
+            of ownership.
+          </p>
+        </section>
+
+        <section>
+          <h1 className="px-6 text-4xl mt-6 md:px-0">ENTIRE AGREEMENT </h1>
+          <p className="px-6 mt-8 leading-loose md:px-0">
+            These terms and conditions, including the policies incorporated
+            herein by express reference, constitute your entire agreement with
+            us with respect to your use of our website.
+          </p>
+        </section>
+      </main>
+      <footer className="text-center mt-16 py-10 bg-slate-100">
+        <div className="container mx-auto">
+          <p className="px-6 text-sm md:px-0">
+            The information on this website has not been evaluated by the Food
+            and Drug Administration. These products are not intended to
+            diagnose, treat, cure or prevent any disease.
+          </p>
+          <p className="px-6 mt-4 text-sm md:px-0">
+            © Endoterec Research 2024 All Rights Reserved. 7780 49th St North
+            Box 513 Pinellas Park FL 33718
+          </p>
+        </div>
+      </footer>
+    </div>
+  );
+}
