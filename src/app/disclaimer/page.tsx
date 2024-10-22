f4d8ae329f9f90145dcf1d99799231d6b13fc13e
Luan Lima <limaluan.dev@gmail.com>
2024-10-21 21:41:48 -0300

    feat: adds disclaimer page

---
+++ src/app/disclaimer/page.tsx
@@ -0,0 +1,214 @@
+/* eslint-disable react/no-unescaped-entities */
+export default function Disclaimer() {
+  return (
+    <div>
+      <main className="container mx-auto mt-6">
+        <section>
+          <h1 className="text-5xl text-center mt-16">Disclaimer</h1>
+          <p className="px-6 mt-16 leading-loose md:px-0">
+            The Endoterec site (the "Site") is an online information service,
+            subject to your compliance with the terms and conditions set forth
+            below. PLEASE READ THIS DOCUMENT CAREFULLY BEFORE ACCESSING OR USING
+            THE SITE. BY ACCESSING OR USING THE SITE, YOU AGREE TO BE BOUND BY
+            THE TERMS AND CONDITIONS SET FORTH BELOW. IF YOU DO NOT WISH TO BE
+            BOUND BY THESE TERMS AND CONDITIONS, YOU MAY NOT ACCESS OR USE THE
+            SITE. Endoterec MAY MODIFY THIS AGREEMENT AT ANY TIME, AND SUCH
+            MODIFICATIONS SHALL BE EFFECTIVE IMMEDIATELY UPON POSTING OF THE
+            MODIFIED AGREEMENT ON THE SITE. YOU AGREE TO REVIEW THE AGREEMENT
+            PERIODICALLY TO BE AWARE OF SUCH MODIFICATIONS AND YOUR OF THE
+            MODIFIED AGREEMENT.
+          </p>
+        </section>
+
+        <section>
+          <h1 className="px-6 text-4xl mt-12 md:px-0">
+            1. Copyright, Licenses and Idea Submissions.
+          </h1>
+          <p className="px-6 mt-8 leading-loose md:px-0">
+            The entire contents of the Site are protected by international
+            copyright and trademark laws. The owner of the copyrights and
+            trademarks are Endoterec, its affiliates or other third party
+            licensors. YOU MAY NOT MODIFY, COPY, REPRODUCE, REPUBLISH, UPLOAD,
+            POST, TRANSMIT, OR DISTRIBUTE, IN ANY MANNER, THE MATERIAL ON THE
+            SITE, INCLUDING TEXT, GRAPHICS, CODE AND/OR SOFTWARE. You may print
+            and download portions of material from the different areas of the
+            Site solely for your own non-commercial use provided that you agree
+            not to change or delete any copyright or proprietary notices from
+            the materials. You agree to grant to Endoterec a non-exclusive,
+            royalty-free, worldwide, perpetual license, with the right to
+            sub-license, to reproduce, distribute, transmit, create derivative
+            works of, publicly display and publicly perform any materials and
+            other information (including, without limitation, ideas contained
+            therein for new or improved products and services) you submit to any
+            public areas of the Site (such as bulletin boards, forums and
+            newsgroups) or by e-mail to Endoterec by all means and in any media
+            now known or hereafter developed. You also grant to Endoterec the
+            right to use your name in connection with the submitted materials
+            and other information as well as in connection with all advertising,
+            marketing and promotional material related thereto. You agree that
+            you shall have no recourse against Endoterec for any alleged or
+            actual infringement or misappropriation of any proprietary right in
+            your communications to Endoterec.
+          </p>
+        </section>
+
+        <section>
+          <h1 className="px-6 text-4xl mt-6 md:px-0">TRADEMARKS.</h1>
+          <p className="px-6 mt-4 leading-loose md:px-0">
+            Publications, products, content or services referenced herein or on
+            the Site are the exclusive trademarks or service marks of Endoterec.
+            Other product and company names mentioned in the Site may be the
+            trademarks of their respective owners.
+          </p>
+        </section>
+
+        <section>
+          <h1 className="px-6 text-4xl mt-6 md:px-0">2. Use of the Site.</h1>
+          <p className="px-6 mt-8 leading-loose md:px-0">
+            You understand that, except for information, products or services
+            clearly identified as being supplied by Endoterec, Endoterec does
+            not operate, control or endorse any information, products or
+            services on the Internet in any way. Except for Endoterec -
+            identified information, products or services, all information,
+            products and services offered through the Site or on the Internet
+            generally are offered by third parties, which are not affiliated
+            with Endoterec. You also understand that Endoterec cannot and does
+            not guarantee or warrant that files available for downloading
+            through the Site will be free of infection or viruses, worms, Trojan
+            horses or other code that manifest contaminating or destructive
+            properties. You are responsible for implementing sufficient
+            procedures and checkpoints to satisfy your particular requirements
+            for accuracy of data input and output, and for maintaining a means
+            external to the Site for the reconstruction of any lost data. YOU
+            ASSUME TOTAL RESPONSIBILITY AND RISK FOR YOUR USE OF THE SITE AND
+            THE INTERNET. Endoterec PROVIDES THE SITE AND RELATED INFORMATION
+            "AS IS" AND DOES NOT MAKE ANY EXPRESS OR IMPLIED WARRANTIES,
+            REPRESENTATIONS OR ENDORSEMENTS WHATSOEVER (INCLUDING WITHOUT
+            LIMITATION WARRANTIES OF TITLE OR NONINFRINGEMENT, OR THE IMPLIED
+            WARRANTIES OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE)
+            WITH REGARD TO THE SERVICE, ANY MERCHANDISE INFORMATION OR SERVICE
+            PROVIDED THROUGH THE SERVICE OR ON THE INTERNET GENERALLY, AND
+            Endoterec SHALL NOT BE LIABLE FOR ANY COST OR DAMAGE ARISING EITHER
+            DIRECTLY OR INDIRECTLY FROM ANY SUCH TRANSACTION. IT IS SOLELY YOUR
+            RESPONSIBILITY TO EVALUATE THE ACCURACY, COMPLETENESS AND USEFULNESS
+            OF ALL OPINIONS, ADVICE, SERVICES, MERCHANDISE AND OTHER INFORMATION
+            PROVIDED THROUGH THE SERVICE OR ON THE INTERNET GENERALLY. DOES NOT
+            WARRANT THAT THE SERVICE WILL BE UNINTERRUPTED OR ERROR-FREE OR THAT
+            DEFECTS IN THE SERVICE WILL BE CORRECTED. YOU UNDERSTAND FURTHER
+            THAT THE PURE NATURE OF THE INTERNET CONTAINS UNEDITED MATERIALS
+            SOME OF WHICH ARE SEXUALLY EXPLICIT OR MAY BE OFFENSIVE TO YOU. YOUR
+            ACCESS TO SUCH MATERIALS IS AT YOUR RISK. Endoterec HAS NO CONTROL
+            OVER AND ACCEPTS NO RESPONSIBILITY WHATSOEVER FOR SUCH MATERIALS.
+          </p>
+        </section>
+
+        <section>
+          <h1 className="px-6 text-4xl mt-6 md:px-0">
+            LIMITATION OF LIABILITY
+          </h1>
+          <p className="px-6 mt-8 leading-loose md:px-0">
+            IN NO EVENT WILL Endoterec BE LIABLE FOR (I) ANY INCIDENTAL,
+            CONSEQUENTIAL, OR INDIRECT DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+            DAMAGES FOR LOSS OF PROFITS, BUSINESS INTERRUPTION, LOSS OF PROGRAMS
+            OR INFORMATION, AND THE LIKE) ARISING OUT OF THE USE OF OR INABILITY
+            TO USE THE SERVICE, OR ANY INFORMATION, OR TRANSACTIONS PROVIDED ON
+            THE SERVICE, OR DOWNLOADED FROM THE SERVICE, OR ANY DELAY OF SUCH
+            INFORMATION OR SERVICE. EVEN Endoterec OR ITS AUTHORIZED
+            REPRESENTATIVES HAVE BEEN ADVISED OF THE POSSIBILITY OF SUCH
+            DAMAGES, OR (II) ANY CLAIM ATTRIBUTABLE TO ERRORS, OMISSIONS, OR
+            OTHER INACCURACIES IN THE SERVICE AND/OR MATERIALS OR INFORMATION
+            DOWNLOADED THROUGH THE SERVICE. BECAUSE SOME STATES DO NOT ALLOW THE
+            EXCLUSION OR LIMITATION OF LIABILITY FOR CONSEQUENTIAL OR INCIDENTAL
+            DAMAGES, THE ABOVE LIMITATION MAY NOT APPLY TO YOU. IN SUCH STATES
+            Daily Health LIABILITY IS LIMITED TO THE GREATEST EXTENT PERMITTED
+            BY LAW. Endoterec makes no representations whatsoever about any
+            other web site which you may access through this one or which may
+            link to this Site. When you access a non- Daily Health web site,
+            please understand that it is independent from Endoterec, and that
+            Endoterec has no control over the content on that web site. In
+            addition, a link to a Endoterec web site does not mean that
+            Endoterec endorses or accepts any responsibility for the content, or
+            the use, of such web site.
+          </p>
+        </section>
+
+        <section>
+          <h1 className="px-6 text-4xl mt-6 md:px-0">3. Indemnification.</h1>
+          <p className="px-6 mt-8 leading-loose md:px-0">
+            You agree to indemnify, defend and hold harmless Endoterec, its
+            officers, directors, employees, agents, licensors, suppliers and any
+            third party information providers to the Service from and against
+            all losses, expenses, damages and costs, including reasonable
+            attorneys' fees, resulting from any violation of this Agreement
+            (including negligent or wrongful conduct) by you or any other person
+            accessing the Service.
+          </p>
+        </section>
+
+        <section>
+          <h1 className="px-6 text-4xl mt-6 md:px-0">
+            4. Third Party Rights.{" "}
+          </h1>
+          <p className="px-6 mt-8 leading-loose md:px-0">
+            The provisions of paragraphs 2 (Use of the Service), and 3
+            (Indemnification) are for the benefit of Endoterec and its officers,
+            directors, employees, agents, licensors, suppliers, and any third
+            party information providers to the Service. Each of these
+            individuals or entities shall have the right to assert and enforce
+            those provisions directly against you on its own behalf.
+          </p>
+        </section>
+
+        <section>
+          <h1 className="px-6 text-4xl mt-6 md:px-0">5.Termination. </h1>
+          <p className="px-6 mt-8 leading-loose md:px-0">
+            This Agreement may be terminated by either party without notice at
+            any time for any reason. The provisions of paragraphs 1 (Copyright,
+            Licenses and Idea Submissions), 2 (Use of the Service), 3
+            (Indemnification), 4 (Third Party Rights) and 6 (Miscellaneous)
+            shall survive any termination of this Agreement.
+          </p>
+        </section>
+
+        <section>
+          <h1 className="px-6 text-4xl mt-6 md:px-0">6. Miscellaneous.</h1>
+          <p className="px-6 mt-8 leading-loose md:px-0">
+            This Agreement shall all be governed and construed in accordance
+            with the laws of The United States of America applicable to
+            agreements made and to be performed in The United States of America.
+            You agree that any legal action or proceeding betweenDaily Health
+            and you for any purpose concerning this Agreement or the parties'
+            obligations hereunder shall be brought exclusively in a federal or
+            state court of competent jurisdiction sitting in The United States
+            of America . Any cause of action or claim you may have with respect
+            to the Service must be commenced within one (1) year after the claim
+            or cause of action arises or such claim or cause of action is
+            barred. Endoterec failure to insist upon or enforce strict
+            performance of any provision of this Agreement shall not be
+            construed as a waiver of any provision or right. Neither the course
+            of conduct between the parties nor trade practice shall act to
+            modify any provision of this Agreement. Daily Health may assign its
+            rights and duties under this Agreement to any party at any time
+            without notice to you.
+          </p>
+          <p className="px-6 mt-4 leading-loose md:px-0 font-bold">
+            Any rights not expressly granted herein are reserved.
+          </p>
+        </section>
+      </main>
+      <footer className="text-center mt-16 py-10 bg-slate-100">
+        <div className="container mx-auto">
+          <p className="px-6 text-sm md:px-0">
+            The information on this website has not been evaluated by the Food
+            and Drug Administration. These products are not intended to
+            diagnose, treat, cure or prevent any disease.
+          </p>
+          <p className="px-6 mt-4 text-sm md:px-0">
+            © Endoterec Research 2024 All Rights Reserved. 7780 49th St North
+            Box 513 Pinellas Park FL 33718
+          </p>
+        </div>
+      </footer>
+    </div>
+  );
+}
