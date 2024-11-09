26d8eff0c927eb2451cf10d5d0f672276c243976
limaluan <limaluan.dev@gmail.com>
2024-11-09 18:20:20 -0300

    adds: Nutrition Facts Dialog

--- src/components/sections/formula/index.tsx
+++ src/components/sections/formula/index.tsx
@@ -1,5 +1,8 @@
 import { OrderButton } from "@/components/order-button";
 import { Subtitle } from "@/components/subtitle";
+import { DialogContent, DialogDescription, DialogTrigger } from "@/components/ui/dialog";
+import { Dialog, DialogTitle } from "@radix-ui/react-dialog";
+import { Search } from "lucide-react";
 
 /* eslint-disable react/no-unescaped-entities */
 export function FormulaSection() {
@@ -41,15 +44,29 @@ export function FormulaSection() {
         />
       </article>
 
-      <img
-        src="plants.png"
-        alt="Endoterec bottle with plants around."
-        className="col-start-1 row-start-1
-      lg:mx-auto lg:w-4/5"
-        loading="lazy"
-        width={364}
-        height={374}
-      />
+      <div className="col-start-1 row-start-1 lg:mx-auto lg:w-4/5 relative">
+        <Dialog>
+          <img
+            src="plants.png"
+            alt="Endoterec bottle with plants around."
+            className="h-full w-full"
+            loading="lazy"
+            width={364}
+            height={374}
+          />
+          <DialogTrigger asChild>
+          <button className="absolute top-[48%] left-[52%] transform -translate-x-1/2 -translate-y-1/2 lg:w-24 lg:h-24 w-20 h-20 rounded-lg bg-brand opacity-60 lg:opacity-45 flex justify-center items-center hover:opacity-90 transition-all">
+            <Search className="w-10 h-10 lg:w-12 lg:h-12 text-white" />
+          </button>
+          </DialogTrigger>
+          <DialogContent className="lg:max-w-7xl">
+            <DialogTitle>Nutrition Facts</DialogTitle>
+            <DialogDescription>
+              <img src="nutrition.png" alt="Nutrition Facts Image" />
+            </DialogDescription>
+          </DialogContent>
+        </Dialog>
+      </div>
 
       <div
         className="text-center not-sr-only
