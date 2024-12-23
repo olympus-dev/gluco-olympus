a26075de7542438d20b18cf3a996b31fdb131879
limaluan <limaluan.dev@gmail.com>
2024-12-23 15:45:46 -0300

    adds: 2 new affiliates note

--- src/app/ds24/affiliates-support/_sections/aff-note.tsx
+++ src/app/ds24/affiliates-support/_sections/aff-note.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import { Ban } from "lucide-react";
 
 export function AffiliatesNotes() {
@@ -8,10 +9,11 @@ export function AffiliatesNotes() {
         <li className="flex gap-4"><Ban className="inline-block text-red-500 min-w-6 min-h-6"/>Do not directly link to our checkout.</li>
         <li className="flex gap-4"><Ban className="inline-block text-red-500 min-w-6 min-h-6"/>Do not send junk traffic.</li>
         <li className="flex gap-4"><Ban className="inline-block text-red-500 min-w-6 min-h-6"/>Do not spam.</li>
-        <li className="flex gap-4"><Ban className="inline-block text-red-500 min-w-6 min-h-6"/>No Brand Bidding!</li>
         <li className="flex gap-4"><Ban className="inline-block text-red-500 min-w-6 min-h-6"/>Do not advertise your own bonuses or anything additional of your own.</li>
         <li className="flex gap-4"><Ban className="inline-block text-red-500 min-w-6 min-h-6"/>Do not claim to represent Endoterec.</li>
         <li className="flex gap-4"><Ban className="inline-block text-red-500 min-w-6 min-h-6"/>Do not advertise incorrect/discounted pricing.</li>
+        <li className="flex gap-4"><Ban className="inline-block text-red-500 min-w-6 min-h-6"/>Affiliates may NOT promote, sell, wholesale or resell Endoterec on any retail site including but not limited to Amazon, eBay and Google Store.</li>
+        <li className="flex gap-4"><Ban className="inline-block text-red-500 min-w-6 min-h-6"/>Affiliates are prohibited from running ads on Google Adwords (or any Google property) that bid on keywords related to "Endoterec." They must not use the term "Endoterec" in any pay-per-click ads (NO BRAND BIDDING!)</li>
         <p className="pt-4">Anyone found doing this will be blacklisted immediately.</p>
       </ul>
     </div>
